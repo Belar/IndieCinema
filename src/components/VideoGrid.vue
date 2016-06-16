cff1a723b468b482eba2465b33092ba37d2ba939
Belar <belar@belardesign.com>
2016-06-16 20:48:23 +0200

    Change modal component tag position in template

--- src/components/VideoGrid.vue
+++ src/components/VideoGrid.vue
@@ -5,7 +5,6 @@
     <button class="button" v-on:click="getList()">Get latest Indie movies</button>
     <input class="new-channel" autofocus autocomplete="off" placeholder="Channel name..." v-model="newChannel" @keyup.enter="addChannel()">
     <button class="button" v-on:click="addChannel()">+</button>
-    <modal :show.sync="showModal" :video.sync="videoModal"></modal>
     <ul>
       <li class="single-movie" v-for="movie in movieList | orderBy 'release_time' -1">
         <div @click="showMovie(movie.uri)">
@@ -24,6 +23,7 @@
     </ul>
   </div>
 
+<modal :show.sync="showModal" :video.sync="videoModal"></modal>
 </template>
 
 <script>
