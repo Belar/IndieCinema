882923a355befd821198ea58f66a52740382dc7a
Belar <belar@belardesign.com>
2016-06-21 18:54:16 +0200

    Add basic logging

--- app-dev.js
+++ app-dev.js
@@ -2,6 +2,7 @@
 
 const Hapi = require('hapi');
 const Inert = require('inert'); // Hapi plugin for file reply
+const Good = require('good'); // Logging
 const WebpackConfig = require('./config/webpack.config.js'); // Webpack config
 const HapiWebpackDevMiddleware = require('hapi-webpack-dev-middleware');
 const HapiWebpackHotMiddleware = require('hapi-webpack-hot-middleware');
@@ -9,74 +10,101 @@ const HapiWebpackHotMiddleware = require('hapi-webpack-hot-middleware');
 
 const server = new Hapi.Server();
 server.connection({
-    port: 3000
+  port: 3000
 });
 
+var logConfig = {
+  reporters: {
+    console: [{
+      module: 'good-squeeze',
+      name: 'Squeeze',
+      args: [{
+        response: '*',
+        log: '*'
+      }]
+    }, {
+      module: 'good-console'
+    }, 'stdout'],
+    file: [{
+      module: 'good-squeeze',
+      name: 'Squeeze',
+      args: [{
+        error: '*'
+      }]
+    }, {
+      module: 'good-squeeze',
+      name: 'SafeJson'
+    }, {
+      module: 'good-file',
+      args: ['./.error_log']
+    }]
+  }
+};
+
 server.register([Inert, {
-    register: HapiWebpackDevMiddleware,
-    options: {
-        config: WebpackConfig,
-        options: {
-            noInfo: true,
-            publicPath: WebpackConfig.output.publicPath,
-            stats: {
-                colors: true
-            }
-        }
-    }
+  register: Good,
+  options: logConfig
 }, {
-    register: HapiWebpackHotMiddleware
+  register: HapiWebpackDevMiddleware,
+  options: {
+    config: WebpackConfig,
+    options: {
+      noInfo: true,
+      publicPath: WebpackConfig.output.publicPath,
+      stats: {
+        colors: true
+      }
+    }
+  }
+}, {
+  register: HapiWebpackHotMiddleware
 }], function (err) {
 
-    if (err) {
-        throw err;
+  if (err) {
+    server.log('error', err);
+  }
+
+  // Just a test route
+  server.route({
+    method: 'GET',
+    path: '/api/call',
+    handler: function (request, reply) {
+      reply({
+        message: 'Hello!'
+      })
     }
+  });
 
-    server.route({
-        method: 'GET',
-        path: '/api/call',
-        handler: function (request, reply) {
-            reply({
-                message: 'Hello!'
-            })
-        }
-    });
+  server.route({
+    method: 'GET',
+    path: '/{filename*}',
+    config: {
+      auth: false,
+      cache: {
+        expiresIn: 24 * 60 * 60 * 1000,
+        privacy: 'public'
+      }
+    },
+    handler: {
+      directory: {
+        path: __dirname + '/public',
+        listing: false,
+        index: false
+      }
+    }
+  });
 
-    server.route({
-        method: 'GET',
-        path: '/{filename*}',
-        config: {
-            auth: false,
-            cache: {
-                expiresIn: 24 * 60 * 60 * 1000,
-                privacy: 'public'
-            }
-        },
-        handler: {
-            directory: {
-                path: __dirname + '/public',
-                listing: false,
-                index: false
-            }
-        }
-    });
   // Load API routes
   server.route(require('./server/routes'));
 
-    server.route({
-        method: 'GET',
-        path: '/',
-        handler: function (request, reply) {
-            reply.file('./public/index.html');
-        }
-    });
 });
 
 
 server.start((err) => {
 
-    if (err) {
-        throw err;
-    }
-    console.log('Server running at:', server.info.uri);
+  if (err) {
+    server.log(['error'], err);
+  }
+  server.log('info', 'Server running at: ' + server.info.uri);
+  
 });
--- app.js
+++ app.js
@@ -2,54 +2,78 @@
 
 const Hapi = require('hapi');
 const Inert = require('inert');
+const Good = require('good'); // Logging
 
 const server = new Hapi.Server();
 server.connection({
-    port: 3000
+  port: 3000
 });
 
-server.register([Inert], function (err) {
+var logConfig = {
+  reporters: {
+    file: [{
+      module: 'good-squeeze',
+      name: 'Squeeze',
+      args: [{
+        error: '*'
+      }]
+    }, {
+      module: 'good-squeeze',
+      name: 'SafeJson'
+    }, {
+      module: 'good-file',
+      args: ['./.error_log']
+    }]
+  }
+};
 
-    if (err) {
-        throw err;
+
+server.register([Inert, {
+  register: Good,
+  options: logConfig
+}], function (err) {
+
+  if (err) {
+    throw err;
+  }
+
+  server.route({
+    method: 'GET',
+    path: '/{filename*}',
+    config: {
+      auth: false,
+      cache: {
+        expiresIn: 24 * 60 * 60 * 1000,
+        privacy: 'public'
+      }
+    },
+    handler: {
+      directory: {
+        path: __dirname + '/public',
+        listing: false,
+        index: false
+      }
     }
+  });
 
-    server.route({
-        method: 'GET',
-        path: '/{filename*}',
-        config: {
-            auth: false,
-            cache: {
-                expiresIn: 24 * 60 * 60 * 1000,
-                privacy: 'public'
-            }
-        },
-        handler: {
-            directory: {
-                path: __dirname + '/public',
-                listing: false,
-                index: false
-            }
-        }
-    });
+  server.route({
+    method: 'GET',
+    path: '/api/call',
+    handler: function (request, reply) {
+      reply({
+        message: 'Hello!'
+      })
+    }
+  });
 
-    server.route({
-        method: 'GET',
-        path: '/api/call',
-        handler: function (request, reply) {
-            reply({
-                message: 'Hello!'
-            })
-        }
-    });
+  server.route({
+    method: 'GET',
+    path: '/',
+    handler: function (request, reply) {
+      reply.file('./public/index.html');
+    }
+  });
 
-    server.route({
-        method: 'GET',
-        path: '/',
-        handler: function (request, reply) {
-            reply.file('./public/index.html');
-        }
-    });
   // Load API routes
   server.route(require('./server/routes'));
   
@@ -58,8 +82,8 @@ server.register([Inert], function (err) {
 
 server.start((err) => {
 
-    if (err) {
-        throw err;
-    }
-    console.log('Server running at:', server.info.uri);
+  if (err) {
+    throw err;
+  }
+  console.log('Server running at:', server.info.uri);
 });
