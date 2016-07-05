68471918d0f1c8128833985ae621cc03b49bb8d5
Belar <belar@belardesign.com>
2016-07-05 19:27:36 +0200

    Remove unneeded styling from App component

--- client/App.vue
+++ client/App.vue
@@ -18,13 +18,6 @@
 
 <style>
   .main-wrapper {
-    color: rgba(0,0,0,.8);
     font-family: Source Sans Pro, Helvetica, sans-serif;
-    text-align: center;
-  }
-
-  a {
-    color: #42b983;
-    text-decoration: none;
   }
 </style>
