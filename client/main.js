623f33b9ef5685fdc456e6f3bd70d5d63f2c7d87
Belar <belar@belardesign.com>
2016-07-13 18:02:23 +0200

    Move vue-resource to entry file

--- client/main.js
+++ client/main.js
@@ -1,5 +1,8 @@
 import Vue from 'vue';
 import App from './App';
+import VueResource from 'vue-resource'; // Web requests, Vue plugin
+
+Vue.use(VueResource);
 
 /* eslint-disable no-new */
 new Vue({
