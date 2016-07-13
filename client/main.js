6eee69d68fdb350293e34a27cbf57c6b923db8a0
Belar <belar@belardesign.com>
2016-07-13 18:03:29 +0200

    Move eslint flag in entry file

--- client/main.js
+++ client/main.js
@@ -1,10 +1,11 @@
+/* eslint-disable */
+
 import Vue from 'vue';
 import App from './App';
 import VueResource from 'vue-resource'; // Web requests, Vue plugin
 
 Vue.use(VueResource);
 
-/* eslint-disable no-new */
 new Vue({
   el: 'body',
   components: {
