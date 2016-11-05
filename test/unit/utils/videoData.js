1d746cbfdec381976faf37eaf44407396a6fafa5
Belar <belar@belardesign.com>
2016-11-05 23:18:40 +0100

    Add example, fake data

---
+++ test/unit/utils/videoData.js
@@ -0,0 +1,19 @@
+/* eslint-disable */
+
+module.exports = [{
+  "uri": "/videos/123451",
+  "name": "Donec risus massa, lacinia sed lorem eu, pellentesque fermentum diam",
+  "description": "Nam sit amet iaculis nisl. Nulla mattis quam a consectetur ullamcorper. Nunc purus leo, varius ac dolor et, congue aliquam risus. Nunc fermentum tincidunt eros. Ut varius cursus lacinia. Donec ligula risus, sagittis nec tincidunt et, faucibus vitae justo. Nulla justo velit, vestibulum eu lectus at, interdum dapibus mauris. Nam at orci lacus. Sed non lectus ligula. Etiam et tempor ex. Nam ut turpis ligula. Ut vitae interdum ex, a eleifend tellus.",
+  "indieCinema": {
+    "order": "1",
+    "channel": "remove"
+  }
+}, {
+  "uri": "/videos/123452",
+  "name": "Vestibulum dapibus lacinia urna luctus porta",
+  "description": "Aenean sollicitudin vitae dolor a consectetur. Maecenas a nisl urna. Proin at magna pharetra, fermentum erat vel, ornare est. Donec in ante ut eros lacinia tincidunt. Duis est metus, consectetur vitae tempus a, pharetra in tellus. Duis facilisis iaculis arcu, in molestie metus. Vivamus elementum nibh felis, id convallis massa luctus eu. Ut sit amet blandit enim. In imperdiet sed ipsum vel rhoncus.",
+  "indieCinema": {
+    "order": "1",
+    "channel": "hide"
+  }
+}];
