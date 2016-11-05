be103f1a7080d82c52e44fcb576082733624617d
Belar <belar@belardesign.com>
2016-11-05 23:35:25 +0100

    Add ChannelGroups spec

---
+++ test/unit/specs/ChannelGroups.spec.js
@@ -0,0 +1,9 @@
+import ChannelGroups from 'client/components/ChannelGroups';
+
+describe('ChannelGroups.vue component', function () {
+  it('should have access to channel groups in store', function () {
+    expect(typeof ChannelGroups.data).to.be.equal('function');
+    var defaultData = ChannelGroups.data();
+    expect(defaultData.sharedState.channelGroups).to.exist;
+  });
+});
