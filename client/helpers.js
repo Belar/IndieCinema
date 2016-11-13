02267f717e5a47c4ce939f3a037b8b09182483fe
Belar <belar@belardesign.com>
2016-11-13 12:37:24 +0100

    Add helpers

---
+++ client/helpers.js
@@ -0,0 +1,18 @@
+module.exports = {
+  removeDuplicates: function (array, prop) {
+    // Accepts array of objects and removes duplicates
+    let uniqueArray = [];
+    let uniqueObject = {};
+    let arrayLength = array.length;
+    let i;
+    // Generate objects identified by prop as key
+    for (i = 0; i < arrayLength; i++) {
+      uniqueObject[array[i][prop]] = array[i];
+    }
+    // Insert objects into array
+    for (i = 0 in uniqueObject) {
+      uniqueArray.push(uniqueObject[i]);
+    }
+    return uniqueArray;
+  }
+};
