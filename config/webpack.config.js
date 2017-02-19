50a56024ca89b6003f906b48a65e5d54824ff6d4
Belar <belar@belardesign.com>
2017-02-19 17:33:24 +0100

    Update configs

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -3,6 +3,7 @@ const webpack = require('webpack')
 const projectRoot = path.resolve(__dirname, '../')
 
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const EslintFriendlyFormatter = require('eslint-friendly-formatter')
 
 module.exports = {
   entry: ['webpack-hot-middleware/client', './client/main.js'],
@@ -12,70 +13,60 @@ module.exports = {
     filename: 'build/build.js'
   },
   resolve: {
-    extensions: ['', '.js', '.vue'],
+    modules: [path.join(__dirname, '../node_modules')],
+    extensions: ['.js', '.vue'],
     alias: {
       'client': path.resolve(__dirname, '../client'),
       'components': path.resolve(__dirname, '../clients/components'),
       'vue$': 'vue/dist/vue.common.js'
     }
   },
-  resolveLoader: {
-    root: path.join(__dirname, 'node_modules'),
-  },
   module: {
-    preLoaders: [{
-      test: /\.vue$/,
-      loader: 'eslint',
+  rules: [
+    {
+      enforce: "pre",
+      test: /\.(js|vue)$/,
+      loader: 'eslint-loader',
       include: projectRoot,
-      exclude: /node_modules/
+      exclude: /node_modules/,
+      options:{
+        formatter: EslintFriendlyFormatter
+      }
+    },
+    {
+      test: /\.vue$/,
+      loader: 'vue-loader',
+      options: {
+        loaders: {
+          'sass': 'vue-style-loader!css-loader!sass-loader'
+        }
+      }
     }, {
       test: /\.js$/,
-      loader: 'eslint',
-      include: projectRoot,
+      loader: 'babel-loader',
       exclude: /node_modules/
-    }],
-    loaders: [{
-      test: /\.vue$/,
-      loader: 'vue'
-    }, {
-      test: /\.js$/,
-      loader: 'babel',
-      exclude: /node_modules/
-    }, {
-      test: /\.json$/,
-      loader: 'json'
     }, {
       test: /\.html$/,
-      loader: 'vue-html'
+      loader: 'vue-html-loader'
     }, {
       test: /\.(png|jpg|gif|svg)$/,
-      loader: 'url',
+      loader: 'url-loader',
       query: {
         limit: 10000,
         name: '[name].[ext]?[hash]'
       }
     }]
   },
-  eslint: {
-    formatter: require('eslint-friendly-formatter')
-  },
-  vue: {
-    loaders: {
-      'sass': 'vue-style!css!sass'
-    }
-  },
   plugins: [
     // https://github.com/glenjamin/webpack-hot-middleware#installation--usage
-    new webpack.optimize.OccurenceOrderPlugin(),
     new webpack.HotModuleReplacementPlugin(),
-    new webpack.NoErrorsPlugin(),
+    new webpack.NoEmitOnErrorsPlugin(),
     // https://github.com/ampedandwired/html-webpack-plugin
     new HtmlWebpackPlugin({
       filename: path.resolve(__dirname, '../public/index.html'),
       template: path.resolve(__dirname, '../build/index_dev.ejs'),
-      inject: true,
-      analytics: false
+      inject: true
     })
   ],
-  devtool: '#eval'
+  devtool: 'eval'
 }
